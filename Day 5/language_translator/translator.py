223df8539c953df3fe9d12e2e32f1ac673ad5edf
Yash Bansal <yashbansal97@gmail.com>
2018-07-10 23:18:33 +0530

    Day 5

---
+++ Day 5/language_translator/translator.py
@@ -0,0 +1,103 @@
+from pickle import load
+from numpy import array
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
+from keras.utils.vis_utils import plot_model
+from keras.models import Sequential
+from keras.layers import LSTM
+from keras.layers import Dense
+from keras.layers import Embedding
+from keras.layers import RepeatVector
+from keras.layers import TimeDistributed
+from keras.callbacks import ModelCheckpoint
+
+
+# load a clean dataset
+def load_clean_sentences(filename):
+    return load(open(filename, 'rb'))
+
+
+# load datasets
+dataset = load_clean_sentences('english-spain-both.pkl')
+train = load_clean_sentences('english-spain-train.pkl')
+test = load_clean_sentences('english-spain-test.pkl')
+
+
+# fit a tokenizer
+def create_tokenizer(lines):
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(lines)
+    return tokenizer
+
+
+# max sentence length
+def max_length(lines):
+    return max(len(line.split()) for line in lines)
+
+# prepare english tokenizer
+eng_tokenizer = create_tokenizer(dataset[:, 0])
+eng_vocab_size = len(eng_tokenizer.word_index) + 1
+eng_length = max_length(dataset[:, 0])
+print('English Vocabulary Size: %d' % eng_vocab_size)
+print('English Max Length: %d' % (eng_length))
+# prepare spaman tokenizer
+spa_tokenizer = create_tokenizer(dataset[:, 1])
+spa_vocab_size = len(spa_tokenizer.word_index) + 1
+spa_length = max_length(dataset[:, 1])
+print('Spanish Vocabulary Size: %d' % spa_vocab_size)
+print('Spanish Max Length: %d' % (spa_length))
+
+# encode and pad sequences
+def encode_sequences(tokenizer, length, lines):
+    # intespa encode sequences
+    X = tokenizer.texts_to_sequences(lines)
+    # pad sequences with 0 values
+    X = pad_sequences(X, maxlen=length, padding='post')
+    return X
+
+# one hot encode target sequence
+def encode_output(sequences, vocab_size):
+    ylist = list()
+    for sequence in sequences:
+        encoded = to_categorical(sequence, num_classes=vocab_size)
+        ylist.append(encoded)
+    y = array(ylist)
+    y = y.reshape(sequences.shape[0], sequences.shape[1], vocab_size)
+    return y
+
+# prepare training data
+trainX = encode_sequences(spa_tokenizer, spa_length, train[:, 1])
+trainY = encode_sequences(eng_tokenizer, eng_length, train[:, 0])
+trainY = encode_output(trainY, eng_vocab_size)
+# prepare validation data
+testX = encode_sequences(spa_tokenizer, spa_length, test[:, 1])
+testY = encode_sequences(eng_tokenizer, eng_length, test[:, 0])
+testY = encode_output(testY, eng_vocab_size)
+
+
+
+
+# define NMT model
+def define_model(src_vocab, tar_vocab, src_timesteps, tar_timesteps, n_units):
+    model = Sequential()
+    model.add(Embedding(src_vocab, n_units, input_length=src_timesteps, mask_zero=True))
+    model.add(LSTM(n_units))
+    model.add(RepeatVector(tar_timesteps))
+    model.add(LSTM(n_units, return_sequences=True))
+    model.add(TimeDistributed(Dense(tar_vocab, activation='softmax')))
+    return model
+
+# define model
+model = define_model(spa_vocab_size, eng_vocab_size, spa_length, eng_length, 256)
+model.compile(optimizer='adam', loss='categorical_crossentropy')
+# summarize defined model
+print(model.summary())
+plot_model(model, to_file='model.png', show_shapes=True)
+
+
+# fit model
+filename = 'model.h5'
+checkpoint = ModelCheckpoint(filename, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+model.fit(trainX, trainY, epochs=30, batch_size=64, validation_data=(testX, testY), callbacks=[checkpoint], verbose=2)
+
